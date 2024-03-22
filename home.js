52d71f36d8cdf68e7b7292963b69dfa7676ce4db
marvellavella <daviniamarvella@gmail.com>
2024-03-22 21:56:19 +0700

    update

--- home.js
+++ home.js
@@ -1,12 +1,12 @@
 function validateForm() {
     var search = document.getElementById("search").value;
 
-    form.addEventListener('submit', function(event) {
-        if (textInput.value.trim() === '') {
-            alert('Write a caption first!');
-            event.preventDefault();
-        }
-        else {
-            alert('Posting...')
-        }
-    });
+    if (textInput.value.trim() === '') {
+        alert('Write a caption first!');
+    }
+    else {
+        alert('Posting...')
+    }
+
+    return true;
+}
